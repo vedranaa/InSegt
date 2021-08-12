aba45338eba4888a1534a1cb5f409b026cba96ea
Vedrana <vand@dtu.dk>
2021-08-12 11:27:17 +0200

    changes to notebooks

--- pycode/annotate_folder_example.py
+++ pycode/annotate_folder_example.py
@@ -9,12 +9,13 @@ Created on Sat Mar 27 17:44:36 2021
 import glob
 import annotator
 
-lenpath = len('/Users/vand/Documents/some_example_data/')
-images = glob.glob(foldername)
+foldername = '/Users/vand/Documents/PROJECTS2/Bone/Originaltdata/MJPR130202 ID8/MJPR130202 ID8_2014-02-18_1503_00/4X-50keV-10W-air-5s/recon/'
+lenpath = len(foldername)
+images = glob.glob(foldername +  '*444.tiff')
 
 for image in images:
     print(f'Annotating image {image}')
     app = annotator.PyQt5.QtWidgets.QApplication([])
     ex = annotator.Annotator.fromFilename(image)
-    #ex.annotationsFilename = image[lenpath:-4] + '_annotations.png'
+    ex.annotationsFilename = image[lenpath:-4] + '_annotations.png'
     app.exec_()
